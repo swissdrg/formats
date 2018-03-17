a2b3da623a40e902cc1c3d47f5a47d111ee4deed
Alain Stulz <alain@appswithlove.com>
2018-03-17 21:57:10 +0100

    Add sign-in verification to some links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312095152) do
+ActiveRecord::Schema.define(version: 20180313085011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20180312095152) do
   create_table "formats", force: :cascade do |t|
     t.string "title"
     t.boolean "multiline"
-    t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
