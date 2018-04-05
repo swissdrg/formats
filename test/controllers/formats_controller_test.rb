946964c061b12c64ef446fb9442cee13fca5f514
Alain Stulz <alain@appswithlove.com>
2018-04-05 18:21:42 +0200

    Added sample data from CSV++ repo

--- test/controllers/formats_controller_test.rb
+++ test/controllers/formats_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class FormatsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get formats_url
+    assert_response :success
+    end
 end
--- test/controllers/preview_controller_test.rb
+++ test/controllers/preview_controller_test.rb
@@ -6,4 +6,6 @@ class PreviewControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+
+
 end
