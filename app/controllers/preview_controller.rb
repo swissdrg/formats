f3300cf18eb4634051cbf7b37aaf63afc8bdc77d
Alain Stulz <alain@appswithlove.com>
2018-04-08 22:22:40 +0200

    Prevent formats without uploads being shown in preview

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -1,17 +1,17 @@
 class PreviewController < ApplicationController
 
   def index
-    # TODO: Filter formats to only display the ones that have an upload
-    # Maybe something like
-    # @formats = Format.joins(:upload).where.not(upload: nil)
-    @formats = Format.all
+    @formats = Format.includes(:upload)
+                   .references(:upload)
+                   .merge(Upload.where.not(:id => nil))
+
+    puts @formats.length
 
     if preview_parameters[:format_id].present? && preview_parameters[:data_sample].present?
       format = Format.find(preview_parameters[:format_id])
       dataSample = preview_parameters[:data_sample]
 
       formatFilePath = format.upload.attachment.file.path
-
       formatFile = File.read(formatFilePath)
 
       @output = CSVPP.parse_str(:input => dataSample, :format => formatFile, :col_sep => '|')
