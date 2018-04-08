cb4e35867f8027828d0f14c34426951efe176b4d
Alain Stulz <alain@appswithlove.com>
2018-04-08 18:18:49 +0200

    Added preview functionality

--- app/assets/javascripts/submit_on_enter.js
+++ app/assets/javascripts/submit_on_enter.js
@@ -1,10 +1,5 @@
 // Submits the form if any input registers that the user pressed the "Enter" key (Event 13)
 $(document).ready(function () {
-    $("input").keypress(function(event) {
-        // console.log(event.which);
-
-    });
-
     $("input").on('keydown', function(e) {
         var keyCode = e.keyCode || e.which;
         if (keyCode == 13) {
--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -1,9 +1,9 @@
 class PreviewController < ApplicationController
+
   def index
     # TODO: Filter formats to only display the ones that have an upload
     # Maybe something like
     # @formats = Format.joins(:upload).where.not(upload: nil)
-
     @formats = Format.all
 
     if preview_parameters[:format_id].present? && preview_parameters[:data_sample].present?
@@ -15,6 +15,12 @@ class PreviewController < ApplicationController
       formatFile = File.read(formatFilePath)
 
       @output = CSVPP.parse_str(:input => dataSample, :format => formatFile, :col_sep => '|')
+
+      if request.xhr?
+        render :json => {
+            :preview => @output
+        }
+      end
     else
       @output = "No preview available."
     end
@@ -25,3 +31,7 @@ class PreviewController < ApplicationController
     params.permit(:data_sample, :format_id)
   end
 end
+
+# 34|foobar|1.1|false
+# 99|hi  there|2.2|100
+# -999|Missing|NA|korrektos
