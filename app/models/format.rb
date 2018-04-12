1262b6e903615e436915ab268b115887a6f73f89
Aless55 <aless1997@windowslive.com>
2018-04-12 10:22:44 +0200

    automatic deleting of all informations by deleting a format

--- app/models/format.rb
+++ app/models/format.rb
@@ -1,6 +1,6 @@
 class Format < ApplicationRecord
-	has_many :variables
+	has_many :variables, dependent: :destroy
 	accepts_nested_attributes_for :variables
 
-  has_one :upload
+  has_one :upload, dependent: :destroy
 end
