372eac7a5c3ca79ae96319c6d09148a5f6a3a272
Alain Stulz <alain@appswithlove.com>
2018-04-30 11:32:31 +0200

    Move logic from preview controller to helper. Mock for generating sample data

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -4,6 +4,7 @@ class PreviewController < ApplicationController
 
   def index
     # Select only formats that have uploads
+    puts request.xhr?
     @formats = Format.all
 
     unless preview_parameters[:format_id].present? && preview_parameters[:data_sample].present?
@@ -12,8 +13,14 @@ class PreviewController < ApplicationController
     end
 
     format = Format.find(preview_parameters[:format_id])
-    preview = generate_preview(format)
-    render json: { preview: preview } if request.xhr?
+    preview = helpers.generate_preview(format, preview_parameters[:data_sample])
+    render json: { preview: preview } if request.xml_http_request?
+  end
+
+  def sample
+    format = Format.find(generate_sample_parameters[:format_id])
+    sample = helpers.generate_sample(format)
+    render json: { sample: sample} if request.xml_http_request?
   end
 
   private
@@ -22,22 +29,9 @@ class PreviewController < ApplicationController
     params.permit(:data_sample, :format_id)
   end
 
-  def generate_preview(format)
-    data_sample = preview_parameters[:data_sample]
-    format_file = File.read(format.attachment.file.path)
-
-    # parse_str method returns an array of hashes
-    output_hashes = CSVPP.parse_str(input: data_sample, format: format_file, col_sep: '|')
-    build_table(output_hashes).to_s
+  def generate_sample_parameters
+    params.permit(:format_id)
   end
 
-  # Use xml builder to convert an array of hashes into an html table
-  def build_table(output_hashes)
-    require 'builder'
-    @xm = Builder::XmlMarkup.new(indent: 2)
-    @xm.table do
-      @xm.tr { output_hashes[0].keys.each { |key| @xm.th(key) } }
-      output_hashes.each { |row| @xm.tr { row.values.each { |value| @xm.td(value) } } }
-    end
-  end
+
 end
---
+++ app/helpers/preview_helper.rb
@@ -0,0 +1,25 @@
+module PreviewHelper
+
+  # Use xml builder to convert an array of hashes into an html table
+  def build_table(output_hashes)
+    require 'builder'
+    xm = Builder::XmlMarkup.new(indent: 2)
+    xm.table do
+      xm.tr { output_hashes[0].keys.each { |key| xm.th(key) } }
+      output_hashes.each { |row| xm.tr { row.values.each { |value| xm.td(value) } } }
+    end
+  end
+
+  def generate_preview(format, data_sample)
+    format_file = File.read(format.attachment.file.path)
+
+    # parse_str method returns an array of hashes
+    output_hashes = CSVPP.parse_str(input: data_sample, format: format_file, col_sep: '|')
+    build_table(output_hashes).to_s
+  end
+
+  def generate_sample(format)
+
+    "sample"
+  end
+end
