a2d98df9efc1fce3c927987b9c3dafdfa5e063d2
Alain Stulz <alain@appswithlove.com>
2018-05-04 16:00:37 +0200

    Rename line_number to Line in Preview view

--- app/helpers/preview_helper.rb
+++ app/helpers/preview_helper.rb
@@ -4,10 +4,14 @@ module PreviewHelper
   def generate_preview(format, data_sample)
     format_file = read_attachment(format)
 
-      # parse_str method returns an array of hashes
-      output_hashes = CSVPP.parse_str(input: data_sample, format: format_file, col_sep: '|')
-      build_table(output_hashes).to_s
+    # parse_str method returns an array of hashes
+    output_hashes = CSVPP.parse_str(input: data_sample, format: format_file, col_sep: '|')
+    # puts output_hashes.inspect
+    output_hashes = output_hashes.map { |arr|
+      arr = {"Line" => arr.delete('line_number')}.merge(arr)
+    }
 
+    build_table(output_hashes).to_s
   end
 
 end
