dc9ca365dcc308ee84b693b46cd2c3aa587bd053
Alain Stulz <alain@appswithlove.com>
2018-05-08 09:52:31 +0200

    Fix reloading JSON Editor in formats/edit

--- app/assets/javascripts/formats.js
+++ app/assets/javascripts/formats.js
@@ -1,4 +1,6 @@
+// var editor = null
 var loadEditor = function () {
+    console.log('loadEditor');
     var container = $('#jsoneditor');
     // Prevent jsoneditor from loading multiple times on reloading page
     if (!container.has('.jsoneditor').length) {
