aa098f8631f367e24b49379a6c71b3e956cfa065
Alain Stulz <alain@appswithlove.com>
2018-05-23 15:08:56 +0200

    Code Review: remove unneeded files
    Code Review: Inline variable

--- test/system/uploads_test.rb
+++
@@ -1,9 +0,0 @@
-require 'application_system_test_case'
-
-class UploadsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit uploads_url
-  #
-  #   assert_selector "h1", text: "Upload"
-  # end
-end
